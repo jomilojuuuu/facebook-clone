aeb0a6604441ad8886a7922c25d7d7e7dc69a05c
Jomiloju Oke <jomilojuforlife2004@gmail.com>
2023-05-10 22:35:43 +0100

    added facebook provider for the login , and then created 3 files✌

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useSession,signIn, signOut} from 'next-auth/react'
+
+const login = () => {
+    return (
+        <div>login</div>
+    )
+}
+
+export default login
